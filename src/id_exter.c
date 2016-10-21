eccb2a7eebc97e4e9440301dbeaaa2bb6e062d18
sparky4 <sparky4@cock.li>
2016-10-21 13:18:58 -0500

    test is working with vl and vh and us wwww

--- src/id_exter.c
+++ src/id_exter.c
@@ -1,16 +1,8 @@
 #include "src/id_exter.h"
 
+#ifndef	__ID_US__
 boolean		NoWait;
 //longword			TimeCount;
-
-boolean	insetupscaling;
-
-byte		far	palette1[256][3],far palette2[256][3];
-unsigned	bufferofs;
-unsigned	displayofs,pelpan;
-
-pictabletype *pictable;
-
 ///////////////////////////////////////////////////////////////////////////
 //
 //	US_CheckParm() - checks to see if a string matches one of a set of
@@ -45,9 +37,24 @@ US_CheckParm(char *parm,char **strings)
 	}
 	return(-1);
 }
+#endif
+#ifndef __ID_VL_H__
+boolean	insetupscaling;
 
+byte		far	palette1[256][3],far palette2[256][3];
+unsigned	bufferofs;
+unsigned	displayofs,pelpan;
 void VL_Plot (int x, int y, int color){}
 void VL_Hlin (unsigned x, unsigned y, unsigned width, unsigned color){}
 void VL_Vlin (int x, int y, int height, int color){}
 void VL_FadeIn (int start, int end, byte far *palette, int steps){}
+#endif
+#ifndef __ID_VH_H__
+pictabletype *pictable;
 int VW_MarkUpdateBlock (int x1, int y1, int x2, int y2){ return 0; }
+#else
+unsigned	uwidthtable[UPDATEHIGH];
+unsigned	wallheight[MAXVIEWWIDTH];
+unsigned	latchpics[NUMLATCHPICS];
+unsigned freelatch = FREESTART;
+#endif
