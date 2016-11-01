28ca99cc1978c11ac5f8fcabf194440d33c6b00e
sparky4 <sparky4@cock.li>
2016-11-01 11:17:43 -0500

    ok wwww

--- awoo.sh
+++ awoo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#for testing
+mv src/WOLF3D.EXE ./wolf3d.exe
+mv *.wl6 ../wolf3d/wolf3d/
 if [ -z "$*" ]; then
 		pee="wwww"
 	else
@@ -6,3 +9,4 @@ if [ -z "$*" ]; then
 fi
 git add .
 . ./boop.sh "$pee"
+mv ../wolf3d/wolf3d/*.wl6 ./
