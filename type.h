af335b97329d08ede7ed61ccf07eb0cd8b6a26af
sparky4 <sparky4@cock.li>
2016-11-09 12:34:32 -0600

    hmm interesting i will work on the memory pointer memptr stuff later not now

--- type.h
+++ type.h
@@ -118,6 +118,101 @@ typedef long fixed;
 
 #define _argv __argv
 #define _argc __argc
+
+#define _BSEG byte _seg
+#define _C_CTSEG t_compscale __far
+#define _C_TSSEG t_compshape __far
 #endif
+#ifdef __BORLANDC__
+#define _BSEG byte _seg
+#define _C_CTSEG t_compscale _seg
+#define _C_TSSEG t_compshape _seg
+#endif
+
 #define BAKAPEEQUIT 	getch(); VL_SetTextMode (); printf("\n_fmemcpy() and VW_Startup breaks. I do not know how to fix this.\n"); MM_Shutdown (); exit(1);
 #endif
+
+/*
+src/contigsc.c:byte _seg	*endscalermemory;
+_BSEG
+src/contigsc.c:	endscalermemory = (void _seg *)(seg+ofs/16);
+_VSEG
+src/contigsc.c:	t_compshape	_seg *shape;
+src/contigsc.c:	t_compshape	_seg *shape;
+_C_TSSEG
+src/id_ca.c:byte 		_seg	*tinf;
+src/id_ca.c:unsigned	_seg	*mapsegs[MAPPLANES];
+_USEG
+src/id_ca.c:maptype		_seg	*mapheaderseg[NUMMAPS];
+_M_TSEG
+src/id_ca.c:byte		_seg	*audiosegs[NUMSNDCHUNKS];
+src/id_ca.c:void		_seg	*grsegs[NUMCHUNKS];
+src/id_ca.c:long		_seg *grstarts;	// array of offsets in egagraph, -1 for sparse
+_LSEG
+src/id_ca.c:long		_seg *audiostarts;	// array of offsets in audio / audiot
+src/id_ca.c:	grstarts = (long _seg *)FP_SEG(&EGAhead);
+src/id_ca.c:	tinf = (byte _seg *)FP_SEG(&maphead);
+src/id_ca.c:		pos = ((mapfiletype	_seg *)tinf)->headeroffsets[i];
+_MF_TSEG
+src/id_ca.c:	audiostarts = (long _seg *)FP_SEG(&audiohead);
+src/id_ca.c:		((mapfiletype _seg *)tinf)->RLEWtag);
+src/id_ca.c:		((mapfiletype _seg *)tinf)->RLEWtag);
+src/id_ca.c:					source = (byte _seg *)bufferseg+(pos-bufferstart);
+src/id_ca.h:extern	byte 		_seg	*tinf;
+src/id_ca.h:extern	unsigned	_seg	*mapsegs[MAPPLANES];
+src/id_ca.h:extern	maptype		_seg	*mapheaderseg[NUMMAPS];
+src/id_ca.h:extern	byte		_seg	*audiosegs[NUMSNDCHUNKS];
+src/id_ca.h:extern	void		_seg	*grsegs[NUMCHUNKS];
+src/id_ca.h:extern long		_seg *grstarts;	// array of offsets in egagraph, -1 for sparse
+src/id_ca.h:extern long		_seg *audiostarts;	// array of offsets in audio / audiot
+src/id_in.c:		byte _seg	*DemoBuffer;
+src/id_in.h:extern	byte _seg	*DemoBuffer;
+src/id_mm.h:typedef void _seg * memptr;
+src/id_pm.c:					_seg *PMSegPages;
+src/id_pm.c:	void			_seg *buf;
+src/id_pm.c:		MM_FreePtr(&(void _seg *)PMSegPages);
+src/id_sd.c:	word		*SoundTable;	// Really * _seg *SoundTable, but that don't work
+src/id_sd.c:		word			_seg *DigiList;
+src/id_vh.c:pictabletype	_seg *pictable;
+_PT_TSEG
+src/id_vh.c:	byte		far *dest, far *srcline, _seg *temp;
+src/id_vh.c:	, fontstruct _seg *font)
+_F_TSEG
+src/id_vh.c:	VWL_MeasureString(string,width,height,(fontstruct _seg *)grsegs[STARTFONT+fontnumber]);
+src/id_vh.c:	VWL_MeasureString(string,width,height,(fontstruct _seg *)grsegs[STARTFONTM+fontnumber]);
+src/id_vh.c:	src = (byte _seg *)grsegs[STARTTILE8];
+src/id_vh.c:	src = (byte _seg *)grsegs[STARTTILE16];
+src/id_vh.c:		src = (byte _seg *)grsegs[STARTTILE16+i];
+src/id_vh.h:extern	pictabletype	_seg *pictable;
+src/id_vh.h:extern	pictabletype	_seg *picmtable;
+src/id_vh.h:extern	spritetabletype _seg *spritetable;
+grep: src/obj: Is a directory
+src/oldscale.c:t_compscale _seg *scaledirectory[MAXSCALEHEIGHT+1];
+src/oldscale.c:t_compscale 	_seg *work;
+src/oldscale.c:	_fmemcpy ((byte _seg *)(*finalspot),(byte _seg *)work,totalsize);
+src/oldscale.c:	t_compshape	_seg *shape;
+src/oldscale.c:	t_compscale _seg *comptable;
+_C_CTSEG
+src/oldscale.c:	t_compshape	_seg *shape;
+src/oldscale.c:	t_compscale _seg *comptable;
+_C_CTSEG
+grep: src/readme: Is a directory
+src/wl_debug.c:	spritetype _seg	*block;
+src/wl_debug.c:extern	word	_seg *DigiList;
+src/wl_def.h:extern	t_compscale _seg *scaledirectory[MAXSCALEHEIGHT+1];
+src/wl_menu.c:	fontstruct _seg *font;
+src/wl_scale.c:t_compscale _seg *scaledirectory[MAXSCALEHEIGHT+1];
+src/wl_scale.c:t_compscale 	_seg *work;
+src/wl_scale.c:	_fmemcpy ((byte _seg *)(*finalspot),(byte _seg *)work,totalsize);
+src/wl_scale.c:	t_compshape	_seg *shape;
+src/wl_scale.c:	t_compscale _seg *comptable;
+_C_CTSEG
+src/wl_scale.c:	t_compshape	_seg *shape;
+src/wl_scale.c:	t_compscale _seg *comptable;
+_CSEG
+src/wl_text.c:	text = (char _seg *)grsegs[artnum];
+src/wl_text.c:	text = (char _seg *)layout;
+src/wl_text.c:	text = (char _seg *)grsegs[artnum];
+src/wl_text.c:	text = (char _seg *)layout;
+
+*/
